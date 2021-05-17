f11276676a8e4f85dd04283d34dade14c236aab4
Ben Holland <bhollan5@kent.edu>
2021-05-17 19:30:53 -0400

    Step 2: Outlining ktty.js

---
+++ version1.0/part_A.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+;
+////  SECTION 1:  Imports.
+
+////  SECTION 2:  App memory. 
+
+////  SECTION 3:  Boot stuff.
+
+////  SECTION 4:  Events.
+
+////  SECTION 5:  Draw functions.
+
+////  SECTION 6:  Algorithms.
