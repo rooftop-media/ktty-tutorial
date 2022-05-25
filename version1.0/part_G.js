0c4da4d98787516801df05fed2496a3b8ef2fe20
bhollan5 <bhollan5@kent.edu>
2022-05-25 14:31:50 -0400

    Update part_G.js

--- version1.0/part_G.js
+++ version1.0/part_G.js
@@ -147,7 +147,7 @@ function Buffer_move_cursor_right() {
 }
 function Buffer_move_cursor_up() {
     var current_x_pos = 1;               /**   To find the xpos of the cursor on the current line.   **/
-    var prev_line_length = 0;            /**   To find the length of the *prev* line, to jump back.  **/
+    var prev_line_length = -1;           /**   To find the length of the *prev* line, to jump back.  **/
     for (var i = 0; i < Buffer.cursor_pos; i++ ) {
         if (Buffer.text[i] == "\n") {
             prev_line_length = current_x_pos;
@@ -156,8 +156,11 @@ function Buffer_move_cursor_up() {
             current_x_pos++;
         }
     }
-    if (prev_line_length > current_x_pos) {        /**   If we're going up **into** a line...        **/
-        Buffer.cursor_pos -= prev_line_length;
+    if (!found_line_start) {                          /**   If there is no next line...                    */
+        Buffer.cursor_pos += current_line_length;
+    } 
+    else if (next_line_length >= current_x_pos) {     /**   If we're going down **into** a line...        **/
+	Buffer.cursor_pos += current_line_length;
     }
     else if (prev_line_length <= current_x_pos) {  /**   If we're going up **above** a line...       **/
         Buffer.cursor_pos -= current_x_pos;
